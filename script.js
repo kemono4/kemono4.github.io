42937212ad4148ae0ae1dc8e12f6f5250fb90223
kemono4 <67272864+kemono4@users.noreply.github.com>
2024-05-10 21:36:53 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+function clearSymbol(){
+  Array.from(document.getElementsByClassName("symbol")).forEach(symbol => {
+    symbol.hidden = true;
+  });
+}
+clearSymbol()
+
+function clearResults(){
+  let result = document.getElementsByClassName('result');
+  let results = Array.from(result);
+  results.forEach(function(element) {
+    element.remove();
+  });
+};
+
 // 清理生成的分身图片
 function clearNoteCopies() {
   // 获取所有的分身图片
@@ -15,18 +30,18 @@ function textToHeight(text) {
   var noteName = text[0].toUpperCase(); // 将字母转换为大写
   // 计算音符的基准高度
   switch (noteName) {
-    case 'C': noteHeight = 146.5; break;
-    case 'D': noteHeight = 140; break;
-    case 'E': noteHeight = 133.5; break;
-    case 'F': noteHeight = 128.5; break;
-    case 'G': noteHeight = 122.5; break;
-    case 'A': noteHeight = 116; break;
-    case 'B': noteHeight = 110; break;
+    case 'C': noteHeight = 140; break;
+    case 'D': noteHeight = 134; break;
+    case 'E': noteHeight = 127.5; break;
+    case 'F': noteHeight = 121; break;
+    case 'G': noteHeight = 115; break;
+    case 'A': noteHeight = 108; break;
+    case 'B': noteHeight = 102.4; break;
     default: noteHeight = 0; // 其他情况的默认高度为 0
    }
    if (text.length > 1) {
     var number = parseInt(text[text.length - 1]); // 获取最后一个字符作为数字
-    noteHeight -= number * 43; // 数字影响音符高度
+    noteHeight -= (number-4) * 44.1;
   }
   return noteHeight;
 }
@@ -55,6 +70,7 @@ function pitch_to_num(name,octave) {
 
 ///////////////////////////
 function check_range(ch, chlen, part, mini, maxi) {
+  range_errors=""
   let first_error=false;
   for (let i = 0; i < chlen; i++) {
     if (ch[i] > maxi || ch[i] < mini) {
@@ -72,6 +88,7 @@ function check_range(ch, chlen, part, mini, maxi) {
 
 // 檢查和聲的函數
 function checkHarmony() {
+  clearResults()
   clearNoteCopies();
   range_errors=''
 
@@ -104,21 +121,16 @@ function checkHarmony() {
 
   if (check_range(channel0,chord_len,'一',60,81)){
     range_errors+="音超過音域"
-}
-
-
-//////////////////////////////
-  let result = document.getElementById('result');
-  result.innerHTML = ''; // 清空之前的結果
-
-  if (range_errors.length > 0) {
-    result.style.color = 'red';
-    result.textContent = range_errors;
-  } else {
-    result.style.color = 'green';
-    result.textContent = "";
+    var errors = document.createElement("div");
+    errors.classList.add("result")
+    errors.innerHTML = range_errors;
+    errors.value = range_errors.slice(5,-5);
+    errors.style.color = 'red'; 
+    document.getElementById('container').appendChild(errors);
   }
-
+  if (check_range(channel1,chord_len,'二',55,74)){
+    range_errors+="音超過音域"
+  }
 
   
 //////////////////////////////////////////////////////
@@ -135,23 +147,27 @@ function checkHarmony() {
 
     // 设置副本图片的 src 为模板图片的 src，实现复制
     copyImage.src = templateImage.src;
-    copyImage.style.width="2.2%";
+    copyImage.style.width="24px";
     // 计算副本图片的位置
     var offsetX
     var offsetY
     offsetX=0
     offsetY=0
-    offsetX = 150 + i * 54; // 每个副本图片在 x 方向上的偏移量
-    offsetY = textToHeight(ch0name[i])+165; // 每个副本图片在 y 方向上的偏移量
+    offsetX = 210 + i*18*48/chord_len; // 每个副本图片在 x 方向上的偏移量
+    offsetY = textToHeight(ch0name[i]); // 每个副本图片在 y 方向上的偏移量
     copyImage.classList.add('note-copy');
     // 设置副本图片的位置和 z-index
     copyImage.style.position = 'absolute'; // 设置为绝对定位
     copyImage.style.top = offsetY + 'px'; // 根据偏移量设置 top
     copyImage.style.left = offsetX + 'px'; // 根据偏移量设置 left
     copyImage.style.zIndex = i + 1; // 根据循环次数设置不同的 z-index
-    copyImage.id = 'copyImage_' + i; 
+    copyImage.id = ('copyImage_' + i.toString()); 
     // 将副本图片添加到页面中
     document.body.appendChild(copyImage);
+    if (ch0name[i].slice(-1)<4 || (ch0name[i].slice(-1)==4 && ch0name[i][0]=='C')){
+      alert('設置下加線')
+
+    }
   };
 
 
@@ -159,56 +175,79 @@ function checkHarmony() {
 }
 
 
-result.addEventListener('click', function() {
-  if (result.style.color === 'blue') {
-    result.style.color = 'red'; 
-    var errorParts = range_errors.slice(5,-5).split(',');
-
-    for (let i = 0; i < errorParts.length; i++) {
-      let copyImage = document.getElementById('copyImage_' + (errorParts[i] - 1).toString());
-      copyImage.src = 'note.png'; 
-    };
-  }else { // 否则当前状态为红色
-
-    result.style.color = 'blue'; 
-    //setTimeout(10)
-
-  // 根据 range_errors 中的信息，标记对应的分身图片
-    var errorParts = range_errors.slice(5,-5).split(',');
+document.addEventListener('click', function(event) {
+  let result = event.target;
+  if (result.classList.contains('result')) {
+    if (result.style.color === 'blue') {
+      result.style.color = 'red'; 
+      var errorParts = result.value.split(',');
   
-    for (let i = 0; i < errorParts.length; i++) {
-      let copyImage = document.getElementById('copyImage_' + (errorParts[i] - 1).toString());
-      copyImage.src = 'staff.jpg'; 
-    };
-  }
+      for (let i = 0; i < errorParts.length; i++) {
+        let copyImage = document.getElementById(('copyImage_' + (errorParts[i] - 1).toString()));
+        copyImage.src = 'note.png'; 
+      };
+    }else { // 否则当前状态为红色
+  
+      result.style.color = 'blue'; 
+      //setTimeout(10)
+  
+    // 根据 range_errors 中的信息，标记对应的分身图片
+      var errorParts = result.value.split(',');
+    
+      for (let i = 0; i < errorParts.length; i++) {
+        let copyImage = document.getElementById(('copyImage_' + (errorParts[i] - 1).toString()));
+        copyImage.src = 'rednote.png'; 
+      };
+    }
+  };
 });
 
 
+//////////////////////////////
 let pname = document.getElementById('pname');
 let key = document.getElementById('key');
-
 
 document.addEventListener('DOMContentLoaded', function() {
   pname.addEventListener('change', updateKey);
   key.addEventListener('change', updateKey);
 })
 const sharp = ['G','D','A','E','B','F#','C#']
+const flat =['F','Bb','Eb','Ab','Db','Gb','Cb']
+const sharpm = ['E','B','F#','C#','G#','D#','A#']
+const flatm =['D','G','C','F','Bb','Eb','Ab']
 
 function updateKey() {
-  Array.from(document.getElementsByClassName("symbol")).forEach(symbol => {
-    symbol.hidden = true;
-  });
+  clearSymbol()
   
   let pnamev = pname.value;
   let keyv = key.value;
   
 
-  if (sharp.includes(pnamev)){
-    if (keyv==""){ //Major
+  if (keyv==""){
+    if (sharp.includes(pnamev)){ //Major
       for (let s=0;s<=sharp.indexOf(pnamev);s++){
         document.getElementById(("sharp"+s.toString())).hidden = false;
+        document.getElementById(("bsharp"+s.toString())).hidden = false;
+      } 
+    }
+    if (flat.includes(pnamev)){
+      for (let s=0;s<=flat.indexOf(pnamev);s++){
+        document.getElementById(("flat"+s.toString())).hidden = false;
+        document.getElementById(("bflat"+s.toString())).hidden = false;
+      } 
+    }
+  }else{
+    if (sharpm.includes(pnamev)){
+      for (let s=0;s<=sharpm.indexOf(pnamev);s++){
+        document.getElementById(("sharp"+s.toString())).hidden = false;
+        document.getElementById(("bsharp"+s.toString())).hidden = false;
+      } 
+    }
+    if (flatm.includes(pnamev)){
+      for (let s=0;s<=flatm.indexOf(pnamev);s++){
+        document.getElementById(("flat"+s.toString())).hidden = false;
+        document.getElementById(("bflat"+s.toString())).hidden = false;
       } 
     }
   }
-
 }
